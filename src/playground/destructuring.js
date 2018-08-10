e0c0afdf68f2bb3def1176da52e7b2f59f9fd3da
gillesMifsud <gil.mifsud@gmail.com>
2018-08-10 14:24:16 +0200

    10-90 Done

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -9,14 +9,14 @@ const person = {
 
  // This creates 2 var person.age and person.name
  const { name, age } = person;
- console.log(`${name} is ${age}`);
+ // console.log(`${name} is ${age}`);
  
  // This changes the name var to firstname and sets default value of Anonymous
  const { name: firstname = 'Anonymous' } = person;
  
 const { city, temp: temperature } = person.location;
 if (city && temperature) {
-    console.log(`It's ${temperature} in ${city}`);
+    // console.log(`It's ${temperature} in ${city}`);
 }
 
 const book = {
@@ -28,4 +28,17 @@ const book = {
 }
 
 const { name: publisherName = 'Self-Published'} = book.publisher;
-console.log(publisherName);
+// console.log(publisherName);
+
+// Array desstrcturing
+// Create an array address
+const address = ['Soi 6', 'Koh Samui', 'Suratthani', '80000'];
+// The position is imoortant
+// name is skippable, leave empty and separate with a comma
+// it can use a default value
+const [road, island, department, zipcode] = address;
+// console.log(`I love ${island} in ${department}`);
+
+const item = ['Coffee (hot)', '2.00€', '2.50€', '2.75€'];
+const [coffeeName, , medium, ] = item;
+console.log(`A ${coffeeName} costs ${medium}`);
--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,16 +1,35 @@
 import { createStore } from 'redux';
 
+// Actions generators = functions that returns action objects
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const resetCount = () => ({
+    type: 'RESET'
+});
+
+const setCount = ({ count } = {}) => ({
+    type: 'SET',
+    count
+});
+
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT' :
-        const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
         };
         case 'DECREMENT' :
-        const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
         };
         case 'SET' :
             return {
@@ -27,31 +46,28 @@ const store = createStore((state = { count: 0 }, action) => {
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
-})
+});
 
-store.dispatch({
+/*store.dispatch({
     type: 'INCREMENT',
     incrementBy: 5
-});
+});*/
 
-store.dispatch({
-    type: 'INCREMENT'
-});
+store.dispatch(incrementCount({incrementBy: 5}));
+store.dispatch(incrementCount());
 
-store.dispatch({
+/*store.dispatch({
     type: 'RESET'
-});
+});*/
+store.dispatch(resetCount());
 
-store.dispatch({
-    type: 'DECREMENT'
-});
+store.dispatch(decrementCount());
+store.dispatch(decrementCount({decrementBy: 10}));
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 10
-});
 
+/*
 store.dispatch({
     type: 'SET',
     count: 101
-});
+});*/
+store.dispatch(setCount({count: 101}));
