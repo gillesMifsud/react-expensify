d88a04a75ec69b0ef39b74cfc3f50645e232bbfe
gillesMifsud <gil.mifsud@gmail.com>
2018-08-11 15:06:48 +0200

    10-91 Done

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -21,28 +21,35 @@ const setCount = ({ count } = {}) => ({
     count
 });
 
-const store = createStore((state = { count: 0 }, action) => {
+// Reducers
+// 1. Reducers are pure functions (return only depends on input)
+// 2. Never change state or action
+
+
+const countReducer = (state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT' :
             return {
                 count: state.count + action.incrementBy
-        };
+            };
         case 'DECREMENT' :
             return {
                 count: state.count - action.decrementBy
-        };
+            };
         case 'SET' :
             return {
                 count: action.count
-        }; 
+            };
         case 'RESET' :
             return {
                 count: 0
-        };    
+            };
         default:
-            return state;    
+            return state;
     }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
