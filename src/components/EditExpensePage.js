8bff484f2e5d9c3cc9bbb4e225f705e8210019a8
Gilles MIFSUD <gilles.mifsud@netapsys.fr>
2018-08-31 07:30:24 +0200

    11-108 Done

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense } from '../actions/expenses';
+import { removeExpense } from "../actions/expenses";
 
 const EditExpensePage = (props) => {
-    console.log(props);
     return (
         <div>
-            <p>Editing the expense with id of {props.match.params.id}</p>
+            <ExpenseForm
+            expense={props.expense}
+            onSubmit={(expense) => {
+                props.dispatch(editExpense(props.expense.id, expense));
+                props.history.push('/');
+            }} 
+        />
+        <button onClick={() => {
+            props.dispatch(removeExpense({
+                id: props.expense.id
+            }));
+            props.history.push('/');
+        }}>Remove</button>
         </div>
     );
 };
 
-export default EditExpensePage;
+// Maps the state to the props we want to use in the particular component
+const mapStateToProps = (state, props) => {
+    return {
+        expense: state.expenses.find((expense) => {
+            return expense.id === props.match.params.id
+        })
+    };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
